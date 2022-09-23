0c00169f192e1fef15257b35335af0dabfcf25db
Billy Dotson <billybad5@gmail.com>
2022-09-23 10:14:12 -0400

    added to program.cs

--- IKBExpenseDb/Program.cs
+++ IKBExpenseDb/Program.cs
@@ -1,15 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddDbContext<AppDbContext>(x => {
+    x.UseSqlServer(builder.Configuration.GetConnectionString("Dev"));
+});
+
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseCors(x => {
+    x.AllowAnyOrigin()
+     .AllowAnyHeader()
+     .AllowAnyMethod();
+});
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services
+            .GetRequiredService<IServiceScopeFactory>()
+            .CreateScope();
+scope.ServiceProvider
+    .GetService<AppDbContext>()!
+    .Database.Migrate();
 
 app.Run();
